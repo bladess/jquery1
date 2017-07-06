381f23031d8abb26c0d5644ff7d9c47946d3684c
nassim <bladess@hotmail.fr>
2017-07-06 14:15:35 +0200

    finish

--- js/app.js
+++ js/app.js
@@ -1,2 +1,10 @@
+$("#addText").html("Hello world");
+$("#remove").remove();
 
+$("#changeColor").addClass("testColor");
+$("#changeColor").html("<div></div>");
+for(var i = 0; i <4;i++){
+	$("#addChildren").html($("#addChildren").html() + "<p></p>");
+}
 console.log('hello world');  
+	
